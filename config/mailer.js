6744da024cabab755eadf2b9a454303ef0ea3260
sjminfotech <elegancewear.in@gmail.com>
2026-06-15 22:55:26 +0530

    fix dotenv and mongo connection issue

--- config/mailer.js
+++ config/mailer.js
@@ -1,14 +1,15 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    pass: process.env.EMAIL_PASS, // app password
   },
+  connectionTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 module.exports = transporter;
