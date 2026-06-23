1dd341ab03067307c8a5eeff001a132b34d80137
sjminfotech <elegancewear.in@gmail.com>
2026-06-23 11:58:49 +0530

    Initial commit or my latest changes

--- routes/premiumAuth.js
+++ routes/premiumAuth.js
@@ -10,7 +10,8 @@ const jwt = require("jsonwebtoken");
 const PremiumAuth = require("../middleware/premiumAuth");
 const adminAuth = require("../middleware/adminAuth");
 const auth = require("../middleware/auth");
-const transporter = require("../config/mailer");
+const Mailer = require("../config/mailer");
+const Resendmailer = require("../config/resendmailer")
 router.post("/signup", async (req, res) => {
     try {
 
