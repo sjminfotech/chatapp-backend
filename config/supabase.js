9f0bf281028bd60576682d84167bc3dc1f8bf06d
sjminfotech <elegancewear.in@gmail.com>
2026-08-04 18:18:40 +0530

    Initial commit

---
+++ config/supabase.js
@@ -0,0 +1,12 @@
+const { createClient } = require("@supabase/supabase-js");
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+if (!supabaseUrl || !supabaseServiceKey) {
+  throw new Error("Supabase URL or Service Role Key is missing in .env");
+}
+
+const supabase = createClient(supabaseUrl, supabaseServiceKey);
+
+module.exports = supabase;
